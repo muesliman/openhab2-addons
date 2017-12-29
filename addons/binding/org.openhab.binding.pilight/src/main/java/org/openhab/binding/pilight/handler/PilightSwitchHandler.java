4b9ee83c4898195df2e40e1dc639f50206cb0020
muesliman <thomas@muesliman.de>
2017-12-29 12:46:28 +0100

    Signed-off-by: muesliman <thomas@muesliman.de>

--- addons/binding/org.openhab.binding.pilight/src/main/java/org/openhab/binding/pilight/handler/PilightSwitchHandler.java
+++ addons/binding/org.openhab.binding.pilight/src/main/java/org/openhab/binding/pilight/handler/PilightSwitchHandler.java
@@ -4,11 +4,17 @@ import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PilightSwitchHandler extends BaseThingHandler {
+    private final Logger logger = LoggerFactory.getLogger(PilightGatewayHandler.class);
+    private PilightGatewayHandler pilightGatewayHandler;
 
     public PilightSwitchHandler(Thing thing) {
         super(thing);
+        this.pilightGatewayHandler = (PilightGatewayHandler) getBridge();
+        logger.debug("bingo");
         // TODO Auto-generated constructor stub
     }
 
